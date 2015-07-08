1f5a31448cbe904cd22ca5aca2406321bd3c0246
Oleksii_Podobed@epam.com <alexey.podobed@gmail.com>
2015-07-08 17:28:38 +0200

    added event model

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -8,4 +8,39 @@ chrome.tabs.onUpdated.addListener(function (tabId) {
   chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+console.log('\'Allo \'Allo! Event Page for Page Action', 1);
+var i = 0;
+
+// One-Time Requests (listening for event names 'dom-loaded')
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+
+  switch(request.type) {
+    case "dom-loaded":
+      console.log(request.data.myProperty + i++);
+      break;
+  }
+  return true;
+});
+
+
+
+//  Long-Lived Connections
+
+chrome.runtime.onConnect.addListener(function(port) {
+
+  if(port.name == "my-channel"){
+    port.onMessage.addListener(function(msg) {
+      // do some stuff here
+      console.log(msg)
+    });
+
+    setTimeout(function () {
+      port.postMessage({lala: "value"});
+    }, 2e3)
+  }
+});
+
+
+
+
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content scripst');
+console.log('\'Allo \'Allo! Content a');
+
+// One-Time Requests (Fire event 'dom-loaded')
+
+window.addEventListener("load", function() {
+  chrome.extension.sendMessage({
+    type: "dom-loaded",
+    data: {
+      myProperty: "value"
+    }
+  });
+}, true);
+
+
+
+
+
+
+
+//  Long-Lived Connections
+
+
+var port = chrome.runtime.connect({name: "my-channel"});
+
+port.postMessage({myProperty: "value"});
+port.onMessage.addListener(function(msg) {
+  console.log('From background:',msg)
+});
